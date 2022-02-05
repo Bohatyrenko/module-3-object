bb2676f54195aed3d0e7cb71f478ec9171614881
Oleksii Bohatyrenko <steenbras31@gmail.com>
2022-02-05 12:54:43 +0200

    add task about hasOwnProperty

--- js/object.js
+++ js/object.js
@@ -4,18 +4,18 @@ const cart = {
     return this.items;
   },
   add(product) {
+    // console.table(this.items);
+    for (const item of this.items) {
+      if (item.name === product.name) {
+        item.quantity += 1;
+        return;
+      }
+    }
     const newProduct = {
       ...product,
       quantity: 1,
     };
     this.items.push(newProduct);
-    // for (const item of this.items) {
-    //   console.log(item);
-    //   // if (item.name === newProduct.name) {
-    //   //   // newProduct.quantity += 1;
-    //   // }
-    //   this.items.push(newProduct);
-    // }
     this.getItems();
   },
   remove(productName) {
@@ -35,16 +35,32 @@ const cart = {
     const { items } = this;
     let totalPrice = 0;
 
-    for (const { price } of items) {
-      totalPrice += price;
+    for (const { price, quantity } of items) {
+      totalPrice += price * quantity;
     }
     return totalPrice;
   },
-  increaseQuantity(productName) {},
-  decreaseQuantity(productName) {},
+  increaseQuantity(productName) {
+    for (const item of this.items) {
+      if (item.name === productName) {
+        item.quantity += 1;
+        return;
+      }
+    }
+  },
+  decreaseQuantity(productName) {
+    for (const item of this.items) {
+      if (item.name === productName) {
+        item.quantity -= 1;
+        return;
+      }
+    }
+  },
 };
 
 cart.add({ name: "apple", price: 50 });
+cart.add({ name: "pear", price: 30 });
+cart.add({ name: "strawberry", price: 110 });
 cart.add({ name: "grape", price: 60 });
 cart.add({ name: "lemon", price: 60 });
 cart.add({ name: "strawberry", price: 110 });
@@ -52,6 +68,23 @@ cart.add({ name: "pear", price: 30 });
 cart.add({ name: "pear", price: 30 });
 // cart.remove("lemon");
 // cart.clear();
-// console.log(cart.countTotalPrice());
-console.log(cart.getItems());
+cart.increaseQuantity("pear");
+cart.increaseQuantity("apple");
+cart.decreaseQuantity("strawberry");
 // console.table(cart.getItems());
+// console.log(cart.countTotalPrice());
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+
+const house = Object.create(apartment);
+house.location = "Kyiv";
+console.log(house.hasOwnProperty("location"));
+for (const key in house) {
+  // console.log(key);
+}
+
+// console.log(house.price);
