e1b5e73baa558cd7a6ced88dddfc886d07dbf3fe
Oleksii Bohatyrenko <steenbras31@gmail.com>
2022-02-08 23:56:04 +0200

    add task # 34

--- js/object.js
+++ js/object.js
@@ -239,21 +239,96 @@
 // console.log(getAllPropValues("quantity"));
 // console.log(getAllPropValues("category"));
 
-function makeTask(data) {
-  const completed = false;
-  const category = "General";
-  const priority = "Normal";
-  console.log(data);
-}
+// function makeTask(data) {
+//   const completed = false;
+//   const category = "General";
+//   const priority = "Normal";
 
-console.log(makeTask({}));
-console.log(
-  makeTask({
-    category: "Homemade",
-    priority: "Low",
-    text: "Take out the trash",
-  })
-);
-console.log(makeTask({ category: "Finance", text: "Take interest" }));
-console.log(makeTask({ priority: "Low", text: "Choose shampoo" }));
-console.log(makeTask({ text: "Buy bread" }));
+//   const newData = {
+//     category,
+//     priority,
+//     completed,
+//     ...data,
+//   };
+//   return newData;
+// }
+
+// console.log(makeTask({}));
+// console.log(
+//   makeTask({
+//     category: "Homemade",
+//     priority: "Low",
+//     text: "Take out the trash",
+//   })
+// );
+// console.log(makeTask({ category: "Finance", text: "Take interest" }));
+// console.log(makeTask({ priority: "Low", text: "Choose shampoo" }));
+// console.log(makeTask({ text: "Buy bread" }));
+
+// function add(...args) {
+//   let total = 0;
+//   for (const el of args) {
+//     total += el;
+//   }
+//   return total;
+// }
+
+// console.log(add(15, 27));
+// console.log(add(12, 4, 11, 48));
+// console.log(add(32, 6, 13, 19, 8));
+
+// function addOverNum(firstArg, ...args) {
+//   let total = 0;
+
+//   for (const arg of args) {
+//     if (arg > firstArg) {
+//       total += arg;
+//     }
+//   }
+
+//   return total;
+// }
+
+// console.log(addOverNum(50, 15, 27));
+// console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
+// console.log(addOverNum(15, 32, 6, 13, 19, 8));
+
+// // Change code below this line
+// function findMatches(firstArg, ...args) {
+//   const matches = []; // Don't change this line
+//   console.log(firstArg);
+//   console.log(args);
+//   for (const el of args) {
+//     for (const number of firstArg) {
+//       if (el === number) {
+//         matches.push(el);
+//       }
+//     }
+//   }
+//   // Change code above this line
+//   return matches;
+// }
+
+// console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
+// console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
+// console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41));
+
+const bookShelf = {
+  // Change code below this line
+  books: ["The last kingdom", "The guardian of dreams"],
+  getBooks() {
+    return "Returning all books";
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook(bookName) {
+    return `Deleting book ${bookName}`;
+  },
+  updateBook(oldName, newName) {
+    return `Updating book ${oldName} to ${newName}`;
+  },
+  // Change code above this line
+};
+
+console.log(bookShelf.removeBook("Red sunset"));
